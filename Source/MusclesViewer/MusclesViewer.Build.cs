c2e9ec8d6b6e235c4476087b841ccec7ab1ef6d8
iantv <ian.tatiana.v@gmail.com>
2021-04-02 13:38:36 +1000

    IKPawn: Set MC in BeginPlay of cpp class

--- Source/MusclesViewer/MusclesViewer.Build.cs
+++ Source/MusclesViewer/MusclesViewer.Build.cs
@@ -8,7 +8,7 @@ public class MusclesViewer : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AnimGraph" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AnimGraph", "HeadMountedDisplay" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
